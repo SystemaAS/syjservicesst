8d27cdd30cacac86bc3318938d625d7f7d253bc8
oscardelatorre <oscardelatorre@localhost>
2021-06-08 14:21:45 +0200

    Update dkxvam for NCTS Export - deleteAll

--- src/no/systema/jservices/skat/nctsexport/model/dao/services/DkxvDaoServicesImpl.java
+++ src/no/systema/jservices/skat/nctsexport/model/dao/services/DkxvDaoServicesImpl.java
@@ -40,6 +40,10 @@ public class DkxvDaoServicesImpl implements DkxvDaoServices {
 			logger.warn(sql.toString());
 			//params
 			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { dao.getTvavd(), dao.getTvtdn() } );
+			if(retval>=0) {
+				logger.warn("deleting children on: dkxvam ...");
+				retval = this.deleteAllChildren(dao.getTvavd(), dao.getTvtdn(), errorStackTrace);
+			}
 			
 		}catch(Exception e){
 			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
@@ -52,6 +56,37 @@ public class DkxvDaoServicesImpl implements DkxvDaoServices {
 		return retval;
 	}
 	
+	/**
+	 * Children tables here
+	 * @param avd
+	 * @param opd
+	 * @param errorStackTrace
+	 * @return
+	 */
+	private int deleteAllChildren(String avd, String opd, StringBuffer errorStackTrace){
+		int retval = 0;
+		try{
+				
+			StringBuffer sql = new StringBuffer();
+			//DEBUG --> logger.info("mydebug");
+			sql.append(" DELETE from dkxvam ");
+			sql.append(" WHERE tvavd = ? ");
+			sql.append(" AND tvtdn = ? ");
+			logger.warn(sql.toString());
+			//params
+			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { avd, opd } );
+			
+			
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = -1;
+		}
+
+		return retval;
+	}
 	/**                                                                                                  
 	 * Wires jdbcTemplate                                                                                
 	 *                                                                                                   
